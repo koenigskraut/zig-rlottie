1379ad9ab2771f7b60511211fb9aa730a86cb292
koenigskraut <koenigskraut1@gmail.com>
2023-10-22 05:00:42 +0300

    update build system and add library dependency

--- build.zig
+++ build.zig
@@ -1,5 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("zig-rlottie", .{ .source_file = .{ .path = "src/library.zig" } });
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const dep = b.dependency("librlottie", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const zig_lottie_module = b.addModule("zig-rlottie", .{ .source_file = .{ .path = "src/library.zig" }, .dependencies = &.{} });
+    _ = zig_lottie_module;
+    const rlottie_lib = dep.artifact("rlottie");
+    _ = rlottie_lib;
 }
