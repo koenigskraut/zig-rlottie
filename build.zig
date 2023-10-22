0504c5f4c6441d695ac4e7fda2b8de95fc98aa2e
koenigskraut <koenigskraut1@gmail.com>
2023-10-22 05:34:19 +0300

    another attempt to fix dependencies

--- build.zig
+++ build.zig
@@ -4,13 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const dep = b.dependency("librlottie", .{
+    const zig_lottie_module = b.addModule("zig-rlottie", .{ .source_file = .{ .path = "src/library.zig" }, .dependencies = &.{} });
+    _ = zig_lottie_module;
+
+    const lib = b.addStaticLibrary(.{
+        .name = "rlottie",
+        .root_source_file = b.addWriteFiles().add("empty.c", ""),
         .target = target,
         .optimize = optimize,
     });
+    link(b, lib);
+    b.installArtifact(lib);
+}
 
-    const zig_lottie_module = b.addModule("zig-rlottie", .{ .source_file = .{ .path = "src/library.zig" }, .dependencies = &.{} });
-    _ = zig_lottie_module;
-    const rlottie_lib = dep.artifact("rlottie");
-    _ = b.addInstallArtifact(rlottie_lib, .{});
+pub fn link(b: *std.Build, step: *std.build.CompileStep) void {
+    const librlottie_dep = b.dependency("librlottie", .{
+        .target = step.target,
+        .optimize = step.optimize,
+    });
+    step.linkLibrary(librlottie_dep.artifact("rlottie"));
 }
